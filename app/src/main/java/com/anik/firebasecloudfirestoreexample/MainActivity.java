93b2402a4c37ef7792364a595e0df1b46a81dd12
Noor Mohammed Anik <noormohammedanik@gmail.com>
2018-09-16 11:23:54 +0600

    Initial commit

---
+++ app/src/main/java/com/anik/firebasecloudfirestoreexample/MainActivity.java
@@ -0,0 +1,126 @@
+package com.anik.firebasecloudfirestoreexample;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editTextName;
+    private EditText editTextBrand;
+    private EditText editTextDesc;
+    private EditText editTextPrice;
+    private EditText editTextQty;
+
+    private FirebaseFirestore db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        db = FirebaseFirestore.getInstance();
+
+        editTextName = findViewById(R.id.edittext_name);
+        editTextBrand = findViewById(R.id.edittext_brand);
+        editTextDesc = findViewById(R.id.edittext_desc);
+        editTextPrice = findViewById(R.id.edittext_price);
+        editTextQty = findViewById(R.id.edittext_qty);
+
+        findViewById(R.id.button_save).setOnClickListener(this);
+        findViewById(R.id.textview_view_products).setOnClickListener(this);
+    }
+
+    private boolean hasValidationErrors(String name, String brand, String desc, String price, String qty) {
+        if (name.isEmpty()) {
+            editTextName.setError("Name required");
+            editTextName.requestFocus();
+            return true;
+        }
+
+        if (brand.isEmpty()) {
+            editTextBrand.setError("Brand required");
+            editTextBrand.requestFocus();
+            return true;
+        }
+
+        if (desc.isEmpty()) {
+            editTextDesc.setError("Description required");
+            editTextDesc.requestFocus();
+            return true;
+        }
+
+        if (price.isEmpty()) {
+            editTextPrice.setError("Price required");
+            editTextPrice.requestFocus();
+            return true;
+        }
+
+        if (qty.isEmpty()) {
+            editTextQty.setError("Quantity required");
+            editTextQty.requestFocus();
+            return true;
+        }
+        return false;
+    }
+
+
+    private void saveProduct(){
+        String name = editTextName.getText().toString().trim();
+        String brand = editTextBrand.getText().toString().trim();
+        String desc = editTextDesc.getText().toString().trim();
+        String price = editTextPrice.getText().toString().trim();
+        String qty = editTextQty.getText().toString().trim();
+
+        if (!hasValidationErrors(name, brand, desc, price, qty)) {
+
+            CollectionReference dbProducts = db.collection("products");
+
+            Product product = new Product(
+                    name,
+                    brand,
+                    desc,
+                    Double.parseDouble(price),
+                    Integer.parseInt(qty)
+            );
+
+            dbProducts.add(product)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Toast.makeText(MainActivity.this, "Product Added", Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId()){
+            case R.id.button_save:
+                saveProduct();
+                break;
+//            case R.id.textview_view_products:
+//                startActivity(new Intent(this, ProductsActivity.class));
+//                break;
+        }
+
+    }
+}
---
+++ app/src/main/java/com/anik/firebasecloudfirestoreexample/Product.java
@@ -0,0 +1,55 @@
+package com.anik.firebasecloudfirestoreexample;
+
+import com.google.firebase.firestore.Exclude;
+
+import java.io.Serializable;
+
+public class Product implements Serializable{
+
+    @Exclude private String id;
+
+    private String name, brand, description;
+    private double price;
+    private int qty;
+
+    public Product() {
+
+    }
+
+    public Product(String name, String brand, String description, double price, int qty) {
+        this.name = name;
+        this.brand = brand;
+        this.description = description;
+        this.price = price;
+        this.qty = qty;
+    }
+
+//    public String getId() {
+//        return id;
+//    }
+
+//    public void setId(String id) {
+//        this.id = id;
+//    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+}
